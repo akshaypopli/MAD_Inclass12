c08e1723424b9fac9b1d433de4a76b9d6cf821ed
Mac <mac@Macs-MacBook-Air.local>
2020-02-10 16:24:09 -0500

    In class 12

---
+++ InClass12/app/src/main/java/com/example/inclass12/Path.java
@@ -0,0 +1,16 @@
+package com.example.inclass12;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Path {
+    @Override
+    public String toString() {
+        return "Path{" +
+                "lat=" + lat +
+                ", lng=" + lng +
+                '}';
+    }
+
+    Double lat, lng;
+
+}
